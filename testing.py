2a20cf48c26e0d5f4b6e535fab4838ff2c214b86
HackXIt <hackxit@gmail.com>
2018-03-05 15:50:13 +0000

    Der Anfang...

---
+++ testing.py
@@ -0,0 +1,9 @@
+"""
+This python program is meant to loop a video from an external flash drive with plug&play functionality.
+The video can either be directly  played if an mp4 file is available or be created from one or more images.
+"""
+
+# Library imports
+import subprocess
+
+subprocess.call('echo "Hello World"', shell=True)
