d2462ff06ee7265213e6dde9c4447928d14dc3ef
HackXIt <hackxit@gmail.com>
2018-03-06 14:52:41 +0000

    Deleted Paragraphs in ls by removing option -Q

--- testing.py
+++ testing.py
@@ -4,6 +4,6 @@ Just testing purposes of short code passages
 # Library imports
 import subprocess
 
-output = subprocess.check_output('ls -d1Q ~/Pictures/*.mp4', shell=True).decode("utf-8")
-
-print('String: %s \n' % type(output), output.split('"'))
+output = subprocess.check_output('ls -d1 ~/Pictures/*.mp4',
+                                 shell=True).decode("utf-8")
+print('String: %s \n' % type(output), output.split('\n'))
