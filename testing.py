3e17006e8dd2e821a92124391e89c3cccb74c2cf
HackXIt <hackxit@gmail.com>
2018-03-07 12:40:22 +0000

    Testing.py: Fetching file path complete. Exception handling works when no .mp4 is found.

--- testing.py
+++ testing.py
@@ -4,6 +4,13 @@ Just testing purposes of short code passages
 # Library imports
 import subprocess
 
-output = subprocess.check_output('ls -d1 ~/Pictures/*.mp4',
-                                 shell=True).decode("utf-8")
-print(output.split('\n')[0])
+def check_file():
+    output = subprocess.check_output('ls -d1 ~/Pictures/*.mp4',
+                                     shell=True).decode("utf-8")
+    return output.split('\n')[0]
+
+try:
+    video = check_file()
+except subprocess.CalledProcessError as lserror:
+    print('Returncode: ', lserror.returncode)
+
