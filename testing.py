627fe38713c87ef57f9fe365375d2806f36d9776
HackXIt <hackxit@gmail.com>
2018-03-07 12:53:20 +0000

    File should be played when HDMI is connected. Will need error Correction and loop break.

--- testing.py
+++ testing.py
@@ -8,9 +8,24 @@ def check_file():
     output = subprocess.check_output('ls -d1 ~/Pictures/*.mp4',
                                      shell=True).decode("utf-8")
     return output.split('\n')[0]
+def play_video(input=None):
+    if input is not None:
+        play = subprocess.run('omxplayer -o hdmi %s &' % input, shell=True)
+        return play
 
 try:
     video = check_file()
-except subprocess.CalledProcessError as lserror:
-    print('Returncode: ', lserror.returncode)
+except subprocess.CalledProcessError as lsError:
+    print('Returncode: ', lsError.returncode)
 
+done = True
+while done:
+    try:
+        done = False
+        if type(play_video(video)) is subprocess.CompletedProcess:
+            print('Playing video finished')
+            done = True
+        else:
+            print('Hmm.')
+    except subprocess.CalledProcessError as playError:
+        print('Returncode: ', lsError.returncode)
