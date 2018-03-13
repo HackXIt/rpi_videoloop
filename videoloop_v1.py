e096c100a008f38f2612141c7ae5a55fada76ff2
HackXIt <hackxit@gmail.com>
2018-03-13 15:18:36 +0100

    Added Version Control.

--- videoloop_v1.py
+++ videoloop_v1.py
@@ -1,11 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.6
 """
 This script loops a video found on attached USB Sticks.
 Rev1: Currently only grabs the first .mp3 file found (Sorted by name)
 Rev2: Restarts loop upon certain exit status (3 = user quit, 1 = failure quit)
 """
 # Library imports
-import subprocess
+import subprocess, sys
+
+# Version Control -> Dependency Python 3.6: subprocess.run()
+if sys.version_info >= (3,6):
+    print("Version OK")
+    version_check_ok = True
+else:
+    print("Missing Dependencies: Python3.6")
+    version_check_ok = False
 
 def check_file():
     output = subprocess.check_output('find /media/ | grep .mp4',
@@ -13,15 +21,18 @@ def check_file():
     return output.split('\n')[0]
 def play_video(input=None):
     if input is not None:
-        play = subprocess.call('omxplayer --aspect-mode fill --vol 0 --loop -o hdmi %s --no-osd' % input, shell=True)
+        play = subprocess.run('omxplayer %s --aspect-mode fill --vol 0 --loop -o hdmi --no-osd' % input, shell=True)
         return play
 
 try:
-    video = check_file()
+    if version_check_ok:
+        video = check_file()
 except subprocess.CalledProcessError as error:
     print('Returncode: ', error.returncode)
-
-check = 3
+if version_check_ok:
+    check = 3
+else:
+    check = 0
 # Infinite Loop for the video, condition triggers when either program fails or user exits by input.
 # Video already loops via omxplayer, this is a failsafe
 while check is not 0:
