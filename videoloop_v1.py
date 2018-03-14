c484bdfcff9b4afaa8ca336c83c99a3e054bb429
HackXIt <hackxit@gmail.com>
2018-03-14 15:07:12 +0100

    Changed return value in play_video -> .returncode. Version loops various video files fine. Still shows CLI upon switching. Fixed some styling issues according to PEP8

--- videoloop_v1.py
+++ videoloop_v1.py
@@ -8,21 +8,25 @@ Rev2: Restarts loop upon certain exit status (3 = user quit, 1 = failure quit)
 import subprocess, sys
 
 # Version Control -> Dependency Python 3.6: subprocess.run()
-if sys.version_info >= (3,6):
+if sys.version_info >= (3, 6):
     print("Version OK")
     version_check_ok = True
 else:
     print("Missing Dependencies: Python3.6")
     version_check_ok = False
 
+
 def check_file():
     output = subprocess.check_output('find /media/ | grep .mp4',
                                      shell=True).decode("utf-8")
     return output.split('\n')
+
+
 def play_video(input=None):
     if input is not None:
-        play = subprocess.run('omxplayer %s --aspect-mode fill --vol 0 -o hdmi --no-osd' % input, shell=True)
-        return play
+        play = subprocess.run('omxplayer %s --aspect-mode fill -o hdmi --no-osd' % input, shell=True)
+        return play.returncode
+
 
 try:
     if version_check_ok:
@@ -33,16 +37,13 @@ if version_check_ok:
     user_exit = False
 else:
     user_exit = True
-	
-print(video)
 # Infinite Loop for the video, condition triggers when either program fails or user exits by input.
-# Video already loops via omxplayer, this is a failsafe
 while not user_exit:
-	for file in video:
-		if file is not '':
-			check = play_video(file)
-			if check is not 0:
-				user_exit = True
-    
-# Exit Code != 0 --> DEBUG: WHY?
-print(check)
+    for file in video:
+        if file is not '':
+            check = play_video(file)
+            if check is not 0:
+                user_exit = True
+            else:
+                user_exit = False
+    print("Restart Loop")
