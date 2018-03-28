ddc6839acf8b23c142e20e56a328a3cf3be2f9e4
HackXIt <hackxit@gmail.com>
2018-03-28 15:18:22 +0200

    Added update script. This is for configuration files ONLY.

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+PATH="/home/pi/rpi_videoloop"
+INIT="/etc/init.d"
+SCRIPT="videoloop_v2.sh"
+CONTROLLER="vid_controller"
+USBCONF="usbmount.conf"
+
+git pull origin master
+
+#Removing existing installation
+if [ -f $INIT/$CONTROLLER]; then
+    cp $CONTROLLER $INIT/
+else
+    echo "No init.d config found."
+fi
+if [ -f $PATH/$SCRIPT]; then
+    cp $SCRIPT $PATH
+else
+    echo "No script found."
+fi
+if [ -f /etc/usbmount/$USBCONF]; then
+    cp $USBCONF /etc/usbmount
+else
+    echo "No usbmount config found."
+fi
