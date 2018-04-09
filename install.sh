3ea0b4d3eb3efc967b63b357affc4ae74cc79641
HackXIt <hackxit@gmail.com>
2018-04-09 16:35:30 +0200

    Adjusted according to code-review suggestions

--- install.sh
+++ install.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
+set -eu
+
+if ! test $UID -eq 0
+then
+   test -t 0 && exec sudo "$0" "$@"
+   # Not a terminal - or exec failed
+   echo "Insufficient privileges - try sudo $0 $*" >&2
+   exit 1
+fi
 
 #Variables
 DEPENDENCIES="omxplayer screen cron usbmount ntfs-3g"
-PTH="/home/pi/rpi_videoloop"
-INIT="/etc/init.d"
+DEPENDENCIES=("${DEPENDENCIES[@]}")
+DESTDIR="/home/pi/rpi_videoloop/"
+INIT="/etc/init.d/"
 SCRIPT="videoloop_v2.sh"
 CONTROLLER="vid_controller"
 USBCONF="usbmount.conf"
-CRONSTUFF=$(cat cron.txt)
+USBDIR="/etc/usbmount/"
+CRONTEXT=$(< cron.txt)
 
 # Checking | Installing Dependancies
-if ! apt list "$DEPENDENCIES" | grep -v installed | grep -E 'omxplayer|screen|cron|usbmount|ntfs-3g' > /dev/null
+if ! apt list "${DEPENDENCIES[*]}" | grep -v installed | grep -E "${DEPENDENCIES[@]// /|}" > /dev/null
 then
 echo "Dependencies already installed. Continuing."
 else
 echo "Installing dependencies."
 apt-get update
-apt-get install "$DEPENDENCIES" -y
+apt-get install "${DEPENDENCIES[*]}" -y
 fi
 
 echo "Installing Configurations..."
 
 #Configuring
-if [ ! -d "$PTH" ]; then 
-    mkdir $PTH
+if [ ! -d "$DESTDIR" ]; then 
+    install -d "$DESTDIR"
 fi
-cp $SCRIPT $PTH
-if [ ! -f $INIT/$CONTROLLER ]; then
-    sudo cp $CONTROLLER $INIT
+install -t "$DESTDIR" "$SCRIPT"
+if [ ! -f "$INIT$CONTROLLER" ]; then
+    install -t "$INIT" "$CONTROLLER"
 fi
-cp $USBCONF /etc/usbmount/
-sudo chmod 755 $PTH/$SCRIPT
-sudo chmod 755 $INIT/$CONTROLLER
-sudo update-rc.d $CONTROLLER defaults
-sudo echo -n " consoleblank=10" | sudo tee -a /boot/cmdline.txt
+install -t "$USBDIR" "$USBCONF"
+update-rc.d "$CONTROLLER" defaults
+printf " consoleblank=10" | sudo tee -a /boot/cmdline.txt
 #cat alias.txt >> ~/.bashrc
 #sudo cat alias.txt >> ~/.bashrc
-(crontab -u pi -l; echo "$CRONSTUFF" ) | crontab -u pi -
+(crontab -u pi -l; echo "$CRONTEXT" ) | crontab -u pi -
 
 echo "FINISHED INSTALLATION: Service control -> /etc/init.d/vid_controller {start|stop|check|repair}"
