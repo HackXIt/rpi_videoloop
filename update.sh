ba3d19a715c5675892878c9cb8c1f86215ff2e38
HackXIt <hackxit@gmail.com>
2018-04-20 20:48:34 +0200

    Fixed cp syntax, wrong address, added chown to videoloop

--- update.sh
+++ update.sh
@@ -1,27 +1,26 @@
 #! /bin/bash
 
-PTH="/home/pi/rpi_videoloop/"
+DESTDIR="/home/pi/rpi_videoloop/"
 INIT="/etc/init.d/"
 SCRIPT="videoloop.sh"
 CONTROLLER="vid_controller"
 USBCONF="usbmount.conf"
 USBDIR="/etc/usbmount/"
 
-git pull origin master
-
 #Replacing existing installation files if existing
 if [ -e "$INIT$CONTROLLER" ]; then
-  cp "$CONTROLLER $INIT"
+  cp "$INIT$CONTROLLER"
 else
   echo "No init.d config found, please use install.sh."
 fi
-if [ -e "$PTH$SCRIPT" ]; then
-  cp "$SCRIPT $PTH"
+if [ -e "$DESTDIR$SCRIPT" ]; then
+  cp "$DESTDIR$SCRIPT"
+  chown pi:pi "$DESTDIR$SCRIPT"
 else
   echo "No script found, please use install.sh."
 fi
 if [ -e "$USBDIR$USBCONF" ]; then
-	cp "$USBCONF $USBDIR"
+	cp "$USBDIR$USBCONF"
 else
 	echo "No usbmount config found, please use install.sh."
 fi
