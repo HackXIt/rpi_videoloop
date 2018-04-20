97f8d864fda780462bb875bc0e320932d51cb3c0
HackXIt <hackxit@gmail.com>
2018-04-20 21:05:20 +0200

    forgot slashes in condition

--- update.sh
+++ update.sh
@@ -8,18 +8,18 @@ USBCONF="usbmount.conf"
 USBDIR="/etc/usbmount"
 
 #Replacing existing installation files if existing
-if [ -e "$INIT$CONTROLLER" ]; then
+if [ -e "$INIT/$CONTROLLER" ]; then
   cp "$CONTROLLER" "$INIT"
 else
   echo "No init.d config found, please use install.sh."
 fi
-if [ -e "$DESTDIR$SCRIPT" ]; then
+if [ -e "$DESTDIR/$SCRIPT" ]; then
   cp "$SCRIPT" "$DESTDIR"
   chown pi:pi "$DESTDIR$SCRIPT"
 else
   echo "No script found, please use install.sh."
 fi
-if [ -e "$USBDIR$USBCONF" ]; then
+if [ -e "$USBDIR/$USBCONF" ]; then
 	cp "$USBCONF" "$USBDIR"
 else
 	echo "No usbmount config found, please use install.sh."
