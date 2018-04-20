611fc30473dc59c2d935eea92ca34927c5bb4ef6
HackXIt <hackxit@gmail.com>
2018-04-20 21:03:26 +0200

    Undo of things cause I'm stupid

--- update.sh
+++ update.sh
@@ -1,26 +1,26 @@
 #! /bin/bash
 
-DESTDIR="/home/pi/rpi_videoloop/"
-INIT="/etc/init.d/"
+DESTDIR="/home/pi/rpi_videoloop"
+INIT="/etc/init.d"
 SCRIPT="videoloop.sh"
 CONTROLLER="vid_controller"
 USBCONF="usbmount.conf"
-USBDIR="/etc/usbmount/"
+USBDIR="/etc/usbmount"
 
 #Replacing existing installation files if existing
 if [ -e "$INIT$CONTROLLER" ]; then
-  cp "$INIT$CONTROLLER"
+  cp "$CONTROLLER" "$INIT"
 else
   echo "No init.d config found, please use install.sh."
 fi
 if [ -e "$DESTDIR$SCRIPT" ]; then
-  cp "$DESTDIR$SCRIPT"
+  cp "$SCRIPT" "$DESTDIR"
   chown pi:pi "$DESTDIR$SCRIPT"
 else
   echo "No script found, please use install.sh."
 fi
 if [ -e "$USBDIR$USBCONF" ]; then
-	cp "$USBDIR$USBCONF"
+	cp "$USBCONF" "$USBDIR"
 else
 	echo "No usbmount config found, please use install.sh."
 fi
