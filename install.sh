fce3ae72918d7989d42bc02827549d266a86069e
HackXIt <n.r.hackx@gmail.com>
2018-10-29 21:20:23 +0100

    Configuration Messages added / improved Crontab configuration

--- install.sh
+++ install.sh
@@ -35,16 +35,25 @@ echo "Installing Configurations..."
 #Configuring
 if [ ! -d "$DESTDIR" ]; then 
   install -d "$DESTDIR"
+  echo "...directory installed..."
+else
+  echo "...directory skipped..."
 fi
 install -t "$DESTDIR" "$SCRIPT"
 if [ ! -f "$INIT$CONTROLLER" ]; then
   install -t "$INIT" "$CONTROLLER"
+  echo "...init script installed..."
+else
+  echo "...init was skipped..."
 fi
 install -t "$USBDIR" "$USBCONF"
+echo "...usbmount configuration installed..."
 update-rc.d "$CONTROLLER" defaults
+echo "...init.d updated..."
 printf " consoleblank=10" | sudo tee -a /boot/cmdline.txt
 #cat alias.txt >> ~/.bashrc
 #sudo cat alias.txt >> ~/.bashrc
-(crontab -u pi -l; echo "$CRONTEXT" ) | crontab -u pi -
+#Exec crontab twice to avoid "No crontab for user" / sort + uniq to avoid double entries
+( (crontab -l 2>/dev/null || echo "")  ; echo "$CRONTEXT") | sort -u - | crontab -
 
 echo "FINISHED INSTALLATION: Service control -> /etc/init.d/vid_controller {start|stop|check|repair}"
