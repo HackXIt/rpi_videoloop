c6cdd5719802893c2266e5cee72ee42f2873e230
HackXIt <hackxit@gmail.com>
2018-10-30 12:59:42 +0100

    Added update messages to update.sh

--- update.sh
+++ update.sh
@@ -10,17 +10,20 @@ USBDIR="/etc/usbmount"
 #Replacing existing installation files if existing
 if [ -e "$INIT/$CONTROLLER" ]; then
   cp "$CONTROLLER" "$INIT"
+  echo "Init.d Controller updated..."
 else
   echo "No init.d config found, please use install.sh."
 fi
 if [ -e "$DESTDIR/$SCRIPT" ]; then
   cp "$SCRIPT" "$DESTDIR"
   chown pi:pi "$DESTDIR/$SCRIPT"
+  echo "Script updated..."
 else
   echo "No script found, please use install.sh."
 fi
 if [ -e "$USBDIR/$USBCONF" ]; then
 	cp "$USBCONF" "$USBDIR"
+  echo "Usbmount updated..."
 else
 	echo "No usbmount config found, please use install.sh."
 fi
